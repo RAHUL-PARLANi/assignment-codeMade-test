3e49f47912bc058c34bd2f703d1f91c721fa3746
RAHUL-PARLANi <90039036+RAHUL-PARLANi@users.noreply.github.com>
2023-11-10 17:56:52 +0530

    Update helloWork.py

--- helloWork.py
+++ helloWork.py
@@ -4,3 +4,19 @@ print('Hello, world!')
 print('Hello, world!')
 print('Hello, world!')
 print('Hello, world!')
+def linear_search(arr, target):
+    for index, value in enumerate(arr):
+        if value == target:
+            return index  # Return the index if the target is found
+    return -1  # Return -1 if the target is not found
+
+# Example usage:
+my_list = [4, 2, 7, 1, 9, 5]
+element_to_find = 7
+
+result = linear_search(my_list, element_to_find)
+
+if result != -1:
+    print(f"Element found at index: {result}")
+else:
+    print("Element not found in the list")
